70a355c82bf9c4b7b58db5788e4a5a6647351b24
Serkan Kurt <serktheturk10@gmail.com>
2013-07-12 09:49:27 -0400

    removed vibration because of missing file: tspdrv.o

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,9 +57,6 @@ BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/hugo/releasetools/hugo_ota_from_target_files
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/hugo/releasetools/hugo_img_from_target_files
 
-# Vibrator
-BOARD_HAS_VIBRATOR_IMPLEMENTATION := ../../device/samsung/hugo/vibrator/tspdrv.c
-
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 
